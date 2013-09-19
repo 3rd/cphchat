e209294285df51eb574be72bb12aa74c1afcebfb
andrew <andrew@it-kartellet.dk>
2013-09-19 23:56:43 +0200

    completed basic ui, added part of the logic

--- webdev/chatclient/core.js
+++ webdev/chatclient/core.js
@@ -1,5 +1,11 @@
-$(function(){
+//globals
+var username="seniorita";
 
+$(function(){
+	drawSentMessage("ce faci scumpule?  <small style='color:#aaa'>12:92</small>");
+	drawReceivedMessage("fanel", "https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-ash3/10438_219232104870483_1529594262_n.jpg", "mulg o capra");
+	drawSentMessage("si ea ce parere are?");
+	drawReceivedMessage("fanel", "https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-ash3/10438_219232104870483_1529594262_n.jpg", "cred ca-i place :)");
 });
 
 //menu toggles
@@ -28,3 +34,40 @@ $("#onlineusers>li:not(.title)").click(function(){
 $("#disconnectBtn").click(function(){
 	//fuck off
 });
+
+$("#sendBtn").click(function(){
+	var text=$("#input").val();
+	//send the shit
+	//display the shit
+	var a=$('#scene').scrollTop()+$("#scene").height()+40;
+	var b=$('#scene')[0].scrollHeight;
+	var toScrollOrNotToScroll=(a==b?true:false);
+	if(toScrollOrNotToScroll) scrollToBottom();
+	drawSentMessage(text);
+	//redrawConversation();
+});
+
+function redrawConversation(){
+	//drawSentMessage(text);
+}
+
+//chat functionality
+function drawSentMessage(message){
+	var dom=$(".template.chat_out").clone().removeClass("template chat_out");
+	dom.find(".avatar span").text(username);
+	dom.find(".content .text").html(message);
+	dom.appendTo($("#chat"));
+}
+function drawReceivedMessage(from, avatar, message){
+	var dom=$(".template.chat_in").clone().removeClass("template chat_out");
+	dom.find(".avatar span").text(from);
+	dom.find(".avatar img").prop("src", avatar);
+	dom.find(".content .text").text(message);
+	dom.appendTo($("#chat"));
+}
+
+function scrollToBottom(){
+	var wtf    = $('#scene');
+	var height = wtf[0].scrollHeight;
+	wtf.scrollTop(height);
+}
